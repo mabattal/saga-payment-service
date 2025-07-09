127110f80f3057167fa97606e90a3c34811cef34
mabattal <mehmetalibattal@gmail.com>
2025-07-09 21:02:36 +0300

    xx

--- src/main/java/com/saga/paymentservice/consumer/PaymentRequestListener.java
+++ src/main/java/com/saga/paymentservice/consumer/PaymentRequestListener.java
@@ -1,18 +1,12 @@
 package com.saga.paymentservice.consumer;
 
 import com.saga.paymentservice.event.PaymentRequestedEvent;
-import com.saga.paymentservice.event.RefundPaymentEvent;
-import com.saga.paymentservice.model.Payment;
-import com.saga.paymentservice.model.PaymentStatus;
-import com.saga.paymentservice.repository.PaymentRepository;
 import com.saga.paymentservice.service.PaymentService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.kafka.support.Acknowledgment;
 import org.springframework.stereotype.Component;
-
-import java.util.Optional;
 
 @Component
 @RequiredArgsConstructor
